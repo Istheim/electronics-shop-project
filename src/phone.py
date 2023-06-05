7e69b4b08b310db44397643815c2e4f50f8aee00
Matvey Zaytsev(Istheim) <zaytsev_matvey98@mail.ru>
2023-06-05 23:32:50 +0700

    add

--- src/phone.py
+++ src/phone.py
@@ -1,11 +1,12 @@
 # Файл: src/phone.py
+# Файл: src/phone.py
 
 from src.item import Item
 
 
 class Phone(Item):
-    def __init__(self, name, quantity, number_of_sim) -> None:
-        super.__init__(name, price, quantity)
+    def __init__(self, name, price, quantity, number_of_sim) -> None:
+        super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
 
     def __str__(self):
@@ -20,7 +21,7 @@ class Phone(Item):
 
     @number_of_sim.setter
     def number_of_sim(self, sim_quantity):
-        if sim_quantity > 0 and isinstance(sim_quantity):
+        if sim_quantity > 0 and isinstance(sim_quantity, int):
             self.__number_of_sim = int(sim_quantity)
         else:
             raise ValueError("Кол-во физ-симок должно быть целым числом больше нуля")
