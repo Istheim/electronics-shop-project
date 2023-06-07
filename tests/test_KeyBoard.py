d829a68090479ca0f4e1c9a0bc3ba73d01c353a9
Matvey Zaytsev(Istheim) <zaytsev_matvey98@mail.ru>
2023-06-07 20:32:02 +0700

    add

---
+++ tests/test_KeyBoard.py
@@ -0,0 +1,18 @@
+def test():
+    # Создаем экземпляр класса для проверки
+    kb = KeyBoard('DarkKD87A', 9600, 5)
+    assert str(kb) == "DarkKD87A"
+    # Проверяем язык по умолчанию
+    assert str(kb.language) == "EN"
+    # Проверяем язык
+    kb.change_lang()
+    assert str(kb.language) == "RU"
+
+    # Меняем РУ на ЕН
+    kb.change_lang().change_lang()
+    assert str(kb.language) == "RU"
+
+    # Проверяем ошибку на язык, только EN & RU
+    with pytest.raises(AttributeError):
+        kb.language = 'CH'
+    # AttributeError: property 'language' of 'KeyBoard' object has no setter
